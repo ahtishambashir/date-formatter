d6bd794c792b87b4ddf4c8fff1d06eaf25364c03
Ahtisham Bashir <ahtishamdoggar@gmail.com>
2025-05-13 18:14:54 +0500

    added the change event listner

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,11 @@ const hours = date.getHours();
 const minutes = date.getMinutes();
 
 const formattedDate = `${day}-${month}-${year}`;
+currentDateParagraph.textContent = formattedDate;
+
+dateOptionsSelectElement.addEventListener("change", () => {
+  switch (dateOptionsSelectElement.value) {
+    case "yyyy-mm-dd":
+      currentDateParagraph.textContent = formattedDate;
+  }
+});
